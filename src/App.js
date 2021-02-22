5ae975557c903dcf8885f03d55e78f9aebe6cf36
Bankole <techybanky@gmail.com>
2021-02-22 07:54:17 +0100

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,40 @@
-import logo from './logo.svg';
-import './App.css';
+// import logo from './logo.svg';
+// import './App.css';
+import { Button, Typography } from "@material-ui/core";
+import { red } from "@material-ui/core/colors";
+import { makeStyles } from "@material-ui/core/styles";
+import { Grid } from "@material-ui/core";
+import Header from "./Header";
+
+const useStyles = makeStyles({
+  testStyle: {
+    color: "red",
+    borderRadius: "50%"
+  }
+});
 
 function App() {
+  const classes = useStyles();
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Grid container direction="column">
+        <Grid item>
+          <Header></Header>
+        </Grid>
+        <Grid item container>
+          <Grid item xs={false} sm={2}></Grid>
+          <Grid item xs={12} sm={8}></Grid>
+          <Grid item xs={false} sm={2}></Grid>
+        </Grid>
+      </Grid>
+      <Button variant="contained" color="warning">
+        This is a button
+      </Button>
+      {/* <Typography color="warning">
+        <h1>Welcome to react</h1>
+        <p>What font is this?</p>
+      </Typography>
+      <button className={classes.testStyle}>Test button</button> */}
     </div>
   );
 }
